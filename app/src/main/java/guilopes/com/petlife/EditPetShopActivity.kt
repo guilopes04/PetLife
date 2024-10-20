b9bbc6f33da2ba7912248add77d11acc0c86e6ac
Guilherme <guilhermelopes035@gmail.com>
2024-10-20 15:33:07 -0300

    feat: implementa EditPetShopActivity e layout para edição de última ida ao pet shop

--- app/src/main/java/guilopes/com/petlife/EditPetShopActivity.kt
+++ app/src/main/java/guilopes/com/petlife/EditPetShopActivity.kt
@@ -1,12 +1,29 @@
 package guilopes.com.petlife
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import guilopes.com.petlife.databinding.ActivityMenuBinding
+import guilopes.com.petlife.databinding.ActivityEditPetShopBinding
 
 class EditPetShopActivity : AppCompatActivity() {
 
-    private val binding: ActivityMenuBinding by lazy {
-        ActivityMenuBinding.inflate(layoutInflater)
+    private val binding: ActivityEditPetShopBinding by lazy {
+        ActivityEditPetShopBinding.inflate(layoutInflater)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportActionBar?.hide()
+        setContentView(binding.root)
+
+        binding.etLastPetShopVisit.setText(MainActivity.pet.lastPetShopVisit)
+
+        binding.btnSave.setOnClickListener {
+
+            MainActivity.pet.lastPetShopVisit = binding.etLastPetShopVisit.text.toString()
+
+            finish()
+        }
+    }
 }
+
