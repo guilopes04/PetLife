6c86e98fdcf4595605bba104ee0ad2ca057cc908
Guilherme <guilhermelopes035@gmail.com>
2024-10-20 14:19:11 -0300

    feat: adiciona classe Pet para armazenar dados do pet

---
+++ app/src/main/java/guilopes/com/petlife/Pet.kt
@@ -0,0 +1,13 @@
+package guilopes.com.petlife
+
+
+data class Pet(
+    var name: String,
+    var birthDate: String,
+    var type: String,
+    var color: String,
+    var size: String,
+    var lastVeterinarianVisit: String,
+    var lastVaccination: String,
+    var lastPetShopVisit: String
+)
